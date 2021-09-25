729338b8ca9a2102e925df05fda20c33f735bb2e
Aman Aggarwal <aman.aggarwal195gmail.com>
2021-09-25 16:28:12 +0530

    internalization code incomp

--- src/main/java/com/rest/restfulwebservices/controllers/HelloWorldController.java
+++ src/main/java/com/rest/restfulwebservices/controllers/HelloWorldController.java
@@ -1,5 +1,6 @@
 package com.rest.restfulwebservices.controllers;
 
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,6 +10,9 @@ import com.rest.restfulwebservices.beans.HelloWorldBean;
 @RestController
 public class HelloWorldController {
 
+	@Autowired
+	MessageSource messageSource;
+	
 	@GetMapping(path="/hello-world")
 	public String helloWorld() {
 		return "Hello World";
@@ -20,5 +24,10 @@ public class HelloWorldController {
 	@GetMapping(path="/hello-world/path-var/{name}")
 	public HelloWorldBean helloWorldBean(@PathVariable String name) {
 		return new HelloWorldBean(String.format("Hello World, %s !!",name));
+	}
+	
+	@GetMapping(path="/hello-world-internalized")
+	public HelloWorldBean helloWorldBean(@RequestHeader(name="Accept-Language") Locale locale) {
+		return new HelloWorldBean(messageSource.getMessage("hello.world", null, locale));
 	}
 }
